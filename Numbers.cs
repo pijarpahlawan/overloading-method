a4c14cb6817235e512bbf5a9b4e54bc5cac89883
pijar <pijarpahlawanq@students.ac.id>
2022-04-05 23:15:59 +0700

    first commit

---
+++ Numbers.cs
@@ -0,0 +1,27 @@
+namespace LatihanOverloadingMethod
+{
+    public class Numbers
+    {
+        private int _a, _b;
+        public int FindMinimum(int number1, int number2)
+        {
+            return number1 < number2 ? number1 : number2;
+        }
+        public int FindMinimum(int number1, int number2, float number3)
+        {
+            _a = number1 < number3 ? number1 : (int)number3;
+            _b = number2 < number3 ? number2 : (int)(number3);
+            return number1 < number2 ? _a : _b;
+        }
+        public int FindMaximum(int number1, int number2)
+        {
+            return number1 > number2 ? number1 : number2;
+        }
+        public int FindMaximum(int number1, int number2, float number3)
+        {
+            _a = number1 > number3 ? number1 : (int)number3;
+            _b = number2 > number3 ? number2 : (int)number3;
+            return number1 > number2 ? _a : _b;
+        }
+    }
+}
