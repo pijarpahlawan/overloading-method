b34e005f8c35725ed5edf2fc8e6ffae72ed766b0
Pijar Pahlawan <95151798+pijarpahlawan@users.noreply.github.com>
2022-04-05 23:20:58 +0700

    fixing typo

--- Numbers.cs
+++ Numbers.cs
@@ -10,7 +10,7 @@ namespace LatihanOverloadingMethod
         public int FindMinimum(int number1, int number2, float number3)
         {
             _a = number1 < number3 ? number1 : (int)number3;
-            _b = number2 < number3 ? number2 : (int)(number3);
+            _b = number2 < number3 ? number2 : (int)number3;
             return number1 < number2 ? _a : _b;
         }
         public int FindMaximum(int number1, int number2)
